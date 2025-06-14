0001f0d89ae803f9f7d496deae2f0233d1fce6d3
jfouret <julien@fouret.me>
2025-06-14 09:02:50 +0200

    Fix plugin download in setup.sh script
    The script now correctly uses the `repo` variable instead of the undefined
    `repository` variable when extracting the organization and name for the plugin
    download. Additionally, it ensures the plugins directory exists before
    attempting to change into it. This fixes the plugin download functionality
    in the setup.sh script.

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-cd $NXF_HOME/plugins
+mkdir -p "$NXF_HOME/plugins"
+
+cd "$NXF_HOME/plugins"
 
 for plugin in jfouret/nf-schema@2.4.2-dev nexomis/nf-nexomis@0.1.0; do
     repo=$(echo $plugin | cut -d'@' -f1)
-    org=$(echo $repository | cut -d'/' -f1)
-    name=$(echo $repository | cut -d'/' -f2)
+    org=$(echo $repo | cut -d'/' -f1)
+    name=$(echo $repo | cut -d'/' -f2)
     ver=$(echo $plugin | cut -d'@' -f2)
     rm -rf $plugin
     wget https://github.com/${org}/${name}/releases/download/${ver}/${name}-${ver}.zip
