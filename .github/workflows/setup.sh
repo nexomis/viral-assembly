4ce82fdd271912493dce822a8804668fa5574d32
jfouret <julien@fouret.me>
2025-06-15 11:13:35 +0200

    Update setup and test scripts for improved

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -2,16 +2,14 @@
 
 mkdir -p "$NXF_HOME/plugins"
 
-cd "$NXF_HOME/plugins"
-
 for plugin in jfouret/nf-schema@2.4.2-dev nexomis/nf-nexomis@0.1.0; do
     repo=$(echo $plugin | cut -d'@' -f1)
     org=$(echo $repo | cut -d'/' -f1)
     name=$(echo $repo | cut -d'/' -f2)
     ver=$(echo $plugin | cut -d'@' -f2)
-    rm -rf $plugin
-    wget https://github.com/${org}/${name}/releases/download/${ver}/${name}-${ver}.zip
-    unzip ${name}-${ver}.zip -d ${name}-${ver}
-    rm ${name}-${ver}.zip
-    rm -rf $plugin
+    wget https://github.com/${org}/${name}/releases/download/${ver}/${name}-${ver}.zip -O $NXF_HOME/plugins/${name}-${ver}.zip
+    unzip $NXF_HOME/plugins/${name}-${ver}.zip -d $NXF_HOME/plugins/${name}-${ver}
+    rm $NXF_HOME/plugins/${name}-${ver}.zip
 done
+
+bash data/test/inputs/get_data_test.sh
